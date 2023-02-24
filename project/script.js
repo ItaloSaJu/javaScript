15b8435f25834746a8517afc6d7cb041ca17fb1c
Italo Salas <italosalas63@gmail.com>
2023-02-24 16:30:37 +0100

    più bello

--- project/script.js
+++ project/script.js
@@ -1,6 +1,8 @@
       
         const f = document.getElementById("fle")
         const to = document.getElementById("toggle")
+        const toco = document.getElementById("toggle-corpo")
+        const toSec2 = document.getElementById("toggle-section2")
         const c = document.getElementById("c")
         const d = document.getElementById("d")
         let darkMode = localStorage.getItem("dark-mode");
@@ -8,8 +10,6 @@
 
         window.addEventListener('scroll', e =>{
             const x = window.pageYOffset;
-            console.log(x);
-            console.log(f);
             if(x >= 350 ){
                  f.style.display= "flex"
             }else if(x <= 550 ){
@@ -28,13 +28,16 @@
 
         const activeDark = () => {
             to.classList.add("invi")
-            to.classList.remove("menu")
+            toco.classList.add("invi")
+            toSec2.classList.add("invi")
             localStorage.setItem("dark-mode", "active")
         }
 
         const disableDark = () => {
             to.classList.remove("invi")
             to.classList.add("menu")
+            toco.classList.remove("invi")
+            toSec2.classList.remove("invi")
             localStorage.setItem("dark-mode", "disabled")
         }
 
@@ -50,3 +53,28 @@
                 disableDark();
             }
         })
+
+        let numeros = [1,2,3,4,5,6]
+
+        let sum = 0;
+        for(var i = 0; i < numeros.length; i++){
+            sum = sum + numeros[i];
+        }
+        console.log(sum);
+
+
+
+
+        
+        var arreglo = [3,6,2,56,5,89,32];
+        var maggiore = 0;
+        var minore = 0;
+        
+        
+            console.log(arreglo.sort());
+            console.log(Math.max(...arreglo))
+            console.log(Math.min(...arreglo))
+            
+        
+       
+        
